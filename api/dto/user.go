c0b769730d7c7e0233ed8672f0c0578f5c4e3a41
Dex <>
2023-11-08 22:11:56 +0800

    bug fixed form json

--- api/dto/user.go
+++ api/dto/user.go
@@ -3,18 +3,18 @@ package dto
 type UserCreateReq struct {
 	Username      string `json:"username"`
 	Password      string `json:"password"`
-	Email         string `gorm:"column:school"`
-	Phone         string `gorm:"column:phone"`
-	Country       string `gorm:"column:country"`
-	Qualification string `gorm:"column:qualification"`
-	Gender        string `gorm:"column:gender"`
+	Email         string `json:"email"`
+	Phone         string `json:"phone"`
+	Country       string `json:"country"`
+	Qualification string `json:"qualification"`
+	Gender        string `json:"gender"`
 }
 
 type UserUpdateReq struct {
 	Id            int64  `json:"id"`
-	Email         string `gorm:"column:school"`
-	Phone         string `gorm:"column:phone"`
-	Country       string `gorm:"column:country"`
-	Qualification string `gorm:"column:qualification"`
-	Gender        string `gorm:"column:gender"`
+	Email         string `json:"email"`
+	Phone         string `json:"phone"`
+	Country       string `json:"country"`
+	Qualification string `json:"qualification"`
+	Gender        string `json:"gender"`
 }
